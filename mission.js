1538c45ffd35146ac506bde3ec033c00f3f2fcf3
skyzobear <machadoalexis@hotmail.fr>
2014-12-09 16:00:31 +0100

    Circuit complet
    Test avec les étapes du circuit complet (réussi)

--- mission.js
+++ mission.js
@@ -1,19 +1,26 @@
 var autonomy = require('ardrone-autonomy');
 var mission  = autonomy.createMission();
 
+
+//circuit
 mission.takeoff()
        .zero()       // Sets the current state as the reference
        .altitude(1)  // Climb to altitude = 1 meter
-       .forward(1.75) 
-       .cw(45)  
-       .forward(1.2)
-       .altitude(1)
-       .ccw(45)
-       .forward(1.7)
 
-       //.cw(270)
-       //.forward(3)
-       //.ctrl.go({x:0, y:0});
+       .forward(1.7) 
+       .cw(50)  
+       .forward(0.8)
+       .altitude(2)
+       .ccw(50)
+       .forward(1.5)
+       .ccw(90)
+       .forward(2.5)
+       .altitude(1)
+       .ccw(100)
+       .forward(1.3)
+       .cw(20)
+       .go({x:0, y:0})
+       .ccw(180)
        .land();
 
 mission.run(function (err, result) {
